1522260f3fec5206df123585c28befca7cef8df3
Jacob Sadowyj <jacob@sadowyj.net>
2025-12-30 16:25:37 -0500

    remove md preview plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -72,16 +72,6 @@ return {
     end,
   },
 
-  -- install without yarn or npm
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function()
-      vim.fn["mkdp#util#install"]()
-    end,
-  },
-
   {
     "ojroques/nvim-bufdel",
     lazy = false,
