6aa14c2bde6b9856daf251f047f20939b0bd7347
Jacob Sadowyj <jacob@sadowyj.net>
2025-12-30 09:46:23 -0500

    replace ui with base46

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,12 +1,8 @@
--- This file needs to have same structure as nvconfig.lua 
--- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
-
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = {
+M.base46 = {
 	theme = "tomorrow_night",
-
 	-- hl_override = {
 	-- 	Comment = { italic = true },
 	-- 	["@comment"] = { italic = true },
